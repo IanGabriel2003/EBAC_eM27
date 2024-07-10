b562501e9ad6bf63723412a522151a499b25d177
Ian Gabriel Pereira de Queiroz <iangabrielpq392@gmail.com>
2024-07-10 18:30:37 -0300

    primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+  data() {
+    return {
+      numero1: 0,
+      numero2: 0,
+      operacao: 'adicionar',
+    };
+  },
+  computed: {
+    resultado() {
+      switch (this.operacao) {
+        case 'adicionar':
+          return this.numero1 + this.numero2;
+        case 'subtrair':
+          return this.numero1 - this.numero2;
+        case 'multiplicar':
+          return this.numero1 * this.numero2;
+        case 'dividir':
+          return this.numero1 / this.numero2;
+        default:
+          return 0;
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <input @keyup="evento => numero1 = (evento.target.value)" type="number" placeholder="Numero 1" name="" id="">
+  <select id="operacao" v-model="operacao">
+    <option value="adicionar">+</option>
+    <option value="subtrair">-</option>
+    <option value="multiplicar">*</option>
+    <option value="dividir">/</option>
+  </select>
+  <input @keyup="evento => numero2 = (evento.target.value)" type="number" placeholder="Numero 2" name="" id="">
+  Resultado: {{ resultado }}
+</template>
+
+<style scoped>
+</style>
